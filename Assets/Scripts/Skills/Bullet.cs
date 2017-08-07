28f0b6881566c02a863b000a0923684bc76c1b39
Balestrieri Julien <julien.balestrieri@gmail.com>
2017-08-07 21:05:59 +0200

    add base class skills

---
+++ Assets/Scripts/Skills/Bullet.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+	public float _speed;
+	public GameObject _target;
+
+	void Update() {
+		Vector3 direction = (_target.transform.position - transform.position).normalized;
+		transform.position += direction * _speed * Time.deltaTime;
+	}
+}
